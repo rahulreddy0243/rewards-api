56a363209e1828dfc461aa28d81f64a821ea8f91
rahulreddy0243 <rahulreddy.j2ee@gmail.com>
2020-07-13 11:16:12 -0500

    Added Model Classes

---
+++ src/main/java/com/customer/rewards/model/ErrorResponse.java
@@ -0,0 +1,33 @@
+package com.customer.rewards.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+	@JsonProperty("errorName")
+	private String errorName;
+
+	@JsonProperty("errorDescription")
+	private String errorDescription;
+
+	public String getErrorName() {
+		return errorName;
+	}
+
+	public void setErrorName(String errorName) {
+		this.errorName = errorName;
+	}
+
+	public String getErrorDescription() {
+		return errorDescription;
+	}
+
+	public void setErrorDescription(String errorDescription) {
+		this.errorDescription = errorDescription;
+	}
+
+}
